8b227cf9d70df58bdbd7dc18001b5e0ab0776ce7
Leandro Giuliani <leandro_giuliani@hotmail.com>
2023-09-11 22:42:27 -0300

    Segundo Commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-const ventamep = 674.18; //Valor de venta dolar mep
-const compramep = 673.50; //Valor de compra dolar mep
 
-alert("Bienvenido a tu simulador de dolar MEP favorito")
+const ventamep = 674.83; // Valor de venta dolar mep
+const compramep = 675.54; // Valor de compra dolar mep
+const ventablue = 717; // Valor de venta dolar blue
+const comprablue = 712; // Valor de compra dolar blue*/
 
-while (true){
-const dolar = prompt("Escribí 'venta' o 'compra' acorde a su consulta sobre dolar MEP. Para salir del menu escribí 'ESC'") .toUpperCase ()
+alert("Bienvenido a tu simulador de dolar favorito");
 
-if (dolar === "COMPRA"){
-alert("El precio de compra es de $" + compramep)
+while (true) {
+  let dolar = prompt("Escribí 'blue' o 'MEP' para información sobre su cotización diaria. Para salir del menú, escribí 'ESC'").toUpperCase();
+
+  if (dolar === "BLUE") {
+    let tipoOperacion = prompt("Escribí 'Compra' o 'Venta' para información sobre su cotización diaria.").toUpperCase();
+
+    if (tipoOperacion === "VENTA") {
+      alert("El precio de venta del dólar blue es de $" + ventablue)
+    } else if (tipoOperacion === "COMPRA") {
+      alert("El precio de compra del dólar blue es de $" + comprablue)
+    } else {
+      alert("Respuesta no válida. Por favor, escribe 'Compra' o 'Venta'.")
+    }
+  } 
+  else if (dolar === "MEP") {
+    let tipoOperacionMEP = prompt("Escribí 'Compra' o 'Venta' para información sobre su cotización diaria.").toUpperCase();
+
+    if (tipoOperacionMEP === "VENTA") {
+      alert("El precio de venta del dólar MEP es de $" + ventamep)
+    } else if (tipoOperacionMEP === "COMPRA") {
+      alert("El precio de compra del dólar MEP es de $" + compramep)
+    } 
+    } else {
+        alert("Saliendo...")
+        break
+  }
 }
-else if(dolar === "VENTA") { 
-alert("El precio de venta es de $" + ventamep)
-}
-else if(dolar === "ESC"){
-    alert("Saliendo..")
-    break;
-}
-else{
-    alert("Respuesta no válida. Por favor escriba 'venta' o 'compra' para recibir la cotización actual o 'ESC' para salir del menu")
-}}
